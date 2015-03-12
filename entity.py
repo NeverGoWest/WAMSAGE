4baf1341dc1d4774da3da052d61a627a63ce9986
Blacklight Shining <blacklightshining@derpymail.org>
2015-03-11 23:12:09 -0700

    Redid entity baseclass (again)
    This time, entity.py features a very minimal Entity class (right now,
    it's just a data class) and a separate Container class. Container inherits from
    the Container, Iterable, and Sized abstract base classes and also implements
    set-like add(), remove(), and discard() methods (indeed, this implementation
    uses an actual set). It also has logging via the `logging` module
    from Python's standard library.

--- entity.py
+++ entity.py
@@ -1,50 +1,52 @@
 # WAMSAGE - West and Moon's Super Awesome Game Engine - entity classes
 
-def printlist(lst):
-    for i in lst:
-        print(i)
-    return lst
+import collections.abc
+import logging
 
 class Entity:
-    
-    def var(self): # easier and more flexible var definitions, keep init for engine specific variables.
-        pass
+    def __init__(self, name, description, container):
+        super().__init__()
+        self.name = name
+        self.description = description
+        self.container = container
+        self._logger = logging.getLogger('entity')
 
-    def __init__(self, src, name, desc, *content): # container, name, description, container, contents
-        self.debug = 1 # debug mode, useful for testing.. and removing bugs.
-        self.name = name # name of the entity
-        self.desc = desc # description of the entity
-        self.src = src # entity source, it's container usually
-        self.cont = content
-        self.var()
+class Container(Entity, collections.abc.Sized, collections.abc.Iterable, collections.abc.Container):
+    def __init__(self, name, description, container, contents):
+        super().__init__(name, description, container)
+        self._contents = set(contents)
 
-    def getcont(self, dbg=0): # returns all the objects found in self.cont
-        contents = [] # the content to be returned
-        contents.extend(self.cont)
-        if dbg:
-            return printlist(contents)
+    def __len__(self):
+        return len(self._contents)
+
+    def __iter__(self):
+        return iter(self._contents)
+
+    def __contains__(self, entity):
+        return entity in self._contents
+
+    def add(self, entity, move=True):
+        if move:
+            entity.container.discard(entity)
+            entity.container = self
+        self._contents.add(entity)
+        self._logger.debug('added entity %s to container %s', entity.name, self.name)
+
+    def remove(self, entity):
+        try:
+            self._contents.remove(entity)
+        except KeyError:
+            self._logger.debug(
+                'entity %s not in container %s, so not removed',
+                entity.name, self.name)
+            raise
         else:
-            return contents
+            self._logger.debug(
+                'entity %s removed from container %s',
+                entity.name, self.name)
 
-    def addcont(self, ent, dbg=0): # add an object to contents of self
-        self.cont.append(ent)
-        if dbg:
-            print("DBG: added {} to {}".format(ent.name, self.name))
-        return self.cont
-
-    def remcont(self, ent, dbg=0): # remove an object from contents of self
-        self.cont.pop(index(ent))
-        if dbg:
-            print("DBG: removed {} from {}".format(ent.name, self.name))
-        return self.cont
-
-    def indcont(self, ent, dbg=0): # find an object in contents of self
-        result = self.cont.index(ent)
-        if dbg:
-            print("DBG: index of {} in {}: {}".format(ent.name, self.name, result))
-        return result
-
-        
-    
-
-
+    def discard(self, entity):
+        try:
+            self.remove(entity)
+        except KeyError:
+            pass
