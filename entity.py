5c3a2d7f10aa2d76dcac3761eb97fb852f06e1c1
NeverGoWest <sejer@nordtun.dk>
2015-03-12 02:14:05 +0100

    Redid entity class
    Now mucb more functional!

--- entity.py
+++ entity.py
@@ -1,9 +1,50 @@
-# DP34TGL - Daev's Python 3.4 Text Game Library - By Dave 2015
+# WAMSAGE - West and Moon's Super Awesome Game Engine - entity classes
 
-class BaseEnt: # Baseclass for any entity, .
-    def __init__(self, source, name, desc):
-        self.name = name
-        self.desc = desc
-    def __str__(self):
-        return "{}\n\n{}".format(self.name, self.desc)
+def printlist(lst):
+    for i in lst:
+        print(i)
+    return lst
+
+class Entity:
+    
+    def var(self): # easier and more flexible var definitions, keep init for engine specific variables.
+        pass
+
+    def __init__(self, src, name, desc, *content): # container, name, description, container, contents
+        self.debug = 1 # debug mode, useful for testing.. and removing bugs.
+        self.name = name # name of the entity
+        self.desc = desc # description of the entity
+        self.src = src # entity source, it's container usually
+        self.cont = content
+        self.var()
+
+    def getcont(self, dbg=0): # returns all the objects found in self.cont
+        contents = [] # the content to be returned
+        contents.extend(self.cont)
+        if dbg:
+            return printlist(contents)
+        else:
+            return contents
+
+    def addcont(self, ent, dbg=0): # add an object to contents of self
+        self.cont.append(ent)
+        if dbg:
+            print("DBG: added {} to {}".format(ent.name, self.name))
+        return self.cont
+
+    def remcont(self, ent, dbg=0): # remove an object from contents of self
+        self.cont.pop(index(ent))
+        if dbg:
+            print("DBG: removed {} from {}".format(ent.name, self.name))
+        return self.cont
+
+    def indcont(self, ent, dbg=0): # find an object in contents of self
+        result = self.cont.index(ent)
+        if dbg:
+            print("DBG: index of {} in {}: {}".format(ent.name, self.name, result))
+        return result
+
+        
+    
+
 
